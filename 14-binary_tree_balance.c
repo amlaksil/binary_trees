f03239ebad96bdb15bd448a768852dd66e0e9967
amlaksil <silamlakdesye@gmail.com>
2023-06-03 00:38:42 +0300

    Measure balance factor to a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,25 @@
 #include "binary_trees.h"
-# define max(x, y) ((x > y) ? x : y)
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height
+ *
+ * Return: height of a tree. If tree is NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t rh, lh;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	return ((lh > rh) ? lh + 1 : rh + 1);
+}
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
@@ -10,40 +30,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int lh = 0, rh = 0, lhr = 0, rhl = 0;
-	binary_tree_t *lptr, *rptr, *lptr_r, *rptr_l;
+	int rh, lh, balance = 0;
 
 	if (!tree)
 		return (0);
-	lptr = tree->left;
-	rptr = tree->right;
-	while (lptr != NULL)
-	{
-		if (lptr->right != NULL)
-		{
-			lptr_r = lptr->right;
-			while (lptr_r != NULL)
-			{
-				lhr++;
-				lptr_r = lptr_r->right;
-			}
-		}
-		lh++;
-		lptr = lptr->left;
-	}
-	while (rptr != NULL)
-	{
-		if (rptr->left != NULL)
-		{
-			rptr_l = rptr->left;
-			while (rptr_l != NULL)
-			{
-				rhl++;
-				rptr_l = rptr_l->left;
-			}
-		}
-		rh++;
-		rptr = rptr->right;
-	}
-	return (max(lh, lhr) - max(rh, rhl));
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	if (!tree->left)
+		lh--;
+	if (!tree->right)
+		rh--;
+	balance = lh - rh;
+	return (balance);
 }
